2f45d7e2c9dcaaa61f84303379f5d643bd63bd6d
Andy Wang <sam.andy.wang@gmail.com>
2022-05-14 00:44:11 -0400

    routes

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
